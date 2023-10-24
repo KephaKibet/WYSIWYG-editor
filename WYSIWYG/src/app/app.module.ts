45997a1dfde0d7242a4b3dbc6be6910e8cde11be
Kepha Kibet <kibet.kepha@live.com>
2023-10-24 19:40:31 +0300

    ngx-wig library

--- WYSIWYG/src/app/app.module.ts
+++ WYSIWYG/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxWigModule } from 'ngx-wig';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxWigModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
