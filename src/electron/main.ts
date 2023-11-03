a09f06e001579c5e7d2abd6a1baec9249c794200
Kepha Kibet <kibet.kepha@live.com>
2023-11-03 13:37:36 +0300

    Interacting with the filesystem

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,12 @@
-import { app, BrowserWindow } from 'electron';
-function createWindow () {
- const mainWindow = new BrowserWindow({
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const contentFile = path.join(app.getPath('userData'), 'content.html');
+
+function createWindow() {
+
+  const mainWindow = new BrowserWindow({
  width: 800,
    height: 600,
    webPreferences: {
@@ -13,3 +19,15 @@ function createWindow () {
 app.whenReady().then(() => {
  createWindow();
 });
+
+ipcMain.handle('getContent', () => {
+  if (fs.existsSync(contentFile)) {
+    const result = fs.readFileSync(contentFile);
+    return result.toString();
+  }
+  return '';
+});
+
+ipcMain.handle('setContent', ({}, content: string) => {
+  fs.writeFileSync(contentFile, content);
+ });
