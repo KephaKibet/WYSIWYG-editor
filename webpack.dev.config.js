78cd4cfb0e47bc0039812b26b7b39339c9f82f5d
Kepha Kibet <kibet.kepha@live.com>
2023-11-03 13:52:20 +0300

    build and package to an executable file

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,10 +3,5 @@ const baseConfig = require('./webpack.config');
 
 module.exports = {
   ...baseConfig,
-  mode: 'development',
-  devtool: 'source-map',
-  output: {
-    path: path.join(process.cwd(), 'dist', 'wysiwyg'),
-    filename: 'shell.js'
-  }
+ 
 };
